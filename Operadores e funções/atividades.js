63af442b3d9de039e06c178aa8c52ad6027944ee
Yago <pavanyago98@gmail.com>
2026-02-12 07:44:15 -0300

    Segunda aula

---
+++ Operadores e funções/atividades.js
@@ -0,0 +1,28 @@
+console.log("*** Atividades ***");
+
+function mostrardatahora() {
+    let data = new Date();
+     console.log(""+data.getHours(), ".", data.getMinutes(), ".", data.getSeconds(), "", data.getDate(), "/", data.getMonth() + 1, "/", data.getFullYear());
+}
+mostrardatahora()
+
+console.log(Math.PI);
+
+function somaMedia(){
+    let num1 = Number(prompt("Digite o primeiro numero:"));
+    let num2 = Number(prompt("Digite o segundo numero:"));
+    let soma = num1 + num2;
+    let media = (soma / 2);
+    console.log("A soma é:", soma);
+    console.log("A média é:", media)
+}
+somaMedia()
+
+function nome() {
+    let nome = prompt("Qual seu nome:");
+    console.log("Tamanho:", nome.length);
+    console.log("Maíusculo:", nome.toUpperCase());
+}
+nome()
+
+
--- Operadores e funções/funcoes.js
+++ Operadores e funções/funcoes.js
@@ -14,6 +14,64 @@ console.log(somar(5, 3));
 
 // atividade
 function imc(peso, altura) {
-    return peso/ altura*altura;
+    return peso / (altura * altura);
 }
-console.log(somar(90,1,90))
+
+console.log(imc(90, 1.90));
+
+function imparpar(num) {
+    if (num % 2 === 0) {
+        return "Par";
+    } else {
+        return "Ímpar";
+    }
+}
+console.log(imparpar(9));
+
+console.log("************** FUNÇÕES NATIVAS **************");
+let agora = new Date();
+console.log(agora);
+
+function mostrardatahora() {
+    let data = new Date();
+     
+    console.log("Dia:", data.getDate());
+    console.log("Mês:", data.getMonth() + 1);
+    console.log("Ano:", data.getFullYear());
+    console.log("Hora:", data.getHours());
+    console.log("Minuto:", data.getMinutes());
+
+    console.log("Desafio: "+data.getHours(), ".", data.getMinutes(), ".", data.getSeconds(), "", data.getDate(), "/", data.getMonth() + 1, "/", data.getFullYear());
+}
+
+mostrardatahora();
+
+console.log(Math.PI);
+
+function calcularOperacoes(numero) {
+    console.log("Raiz:", Math.sqrt(numero));
+    console.log("Arredondado:", Math.round(numero));
+    console.log("Para cima:", Math.ceil(numero));
+    console.log("Para baixo:", Math.floor(numero));
+    console.log("Quadrado:", Math.pow(numero, 2));
+    console.log("Absoluto:", Math.abs(numero))
+}
+calcularOperacoes(7.8)
+
+console.log("************** Funções string **************");
+function analisartexto(texto) {
+    console.log("Tamanho:", texto.length);
+    console.log("Maíusculo:", texto.toUpperCase());
+    console.log("Minúsculo:", texto.toLowerCase());
+}
+analisartexto("JavaScript");
+
+function verificacao(frase) {
+    console.log(frase.includes("JavaScript"));
+}
+verificacao("Eu estudo JavaScript");
+
+function juntastring(nome, curso) {
+    return "Aluno: " + nome + " | Curso: " + curso;
+}
+console.log(juntastring("José", "Front-End"));
