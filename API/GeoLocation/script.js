570092bda02fed78911623e636fc1c417283a534
Yago de Lima Pavan <pavanyago98@gmail.com>
2026-09-03 11:50:54 -0300

    GeoLocation e Camera

--- API/GeoLocation/script.js
+++ API/GeoLocation/script.js
@@ -1,10 +1,17 @@
+let imagem = document.querySelector("#resposta")
+
+let localizacaoAtiva = false;
+
 navigator.geolocation.getCurrentPosition(
     function (posicao) {
-        console.log("Latitude: ",posicao.coords.Latitude);
-        console.log("Longitude: ",posicao.coords.Longitude); 
-        console.log("Precisão: ", posicao.coords.accuracy);
+        console.log("Latitude:", posicao.coords.latitude);
+        console.log("Logintude:", posicao.coords.longitude);
+        console.log("Precisão:", posicao.coords.accuracy);
+        imagem.src = "https://cdn-icons-png.flaticon.com/512/9776/9776798.png";
+
     },
-    function (erro){
-        console.log("Não foi possível obter sua localização")
+    function (erro) {
+        console.log("Não foi possível obter a localização.", erro);
+        imagem.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMOazILpYLFDKdhYB-afCl3AXCnChNOJYs_8DFyqbMTg&s=10.com";
     }
-)
+);
